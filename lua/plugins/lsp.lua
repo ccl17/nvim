41c19cf51aff44ade3013225b5dd06e8c6af7c57
Samuel <chenlu.ccl@gmail.com>
2023-09-12 00:43:53 +0800

    increase test output size

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -178,6 +178,8 @@ local M = {
       require("go").setup({
         auto_format = false,
         auto_lint = false,
+        test_popup_width = 200,
+        test_popup_height = 100,
       })
 
       local wk = require("which-key")
