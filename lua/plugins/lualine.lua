1c9331ca37603b223a69774c474a19c279dbdc39
Samuel <chenlu.ccl@gmail.com>
2023-09-16 23:21:45 +0800

    show lualine icon format on save

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -12,7 +12,19 @@ local M = {
   },
   config = function()
     require("lualine").setup({
-      sections = { lualine_c = { { "filename", path = 1 } } },
+      sections = {
+        lualine_c = { { "filename", path = 1 } },
+        -- lualine_y = { function() vim.b.format_on_save end }
+        lualine_y = {
+          function()
+            if vim.b.format_on_save then
+              return " "
+            else
+              return " "
+            end
+          end,
+        },
+      },
     })
   end,
 }
